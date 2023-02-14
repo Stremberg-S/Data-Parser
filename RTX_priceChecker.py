2d3180937f70ba5638e667df383fad44de8c2cd3
Sergei Stremberg <sekispb@hotmail.com>
2023-02-14 16:06:30 +0200

    little changes to get_price() method due changes on target website

--- RTX_priceChecker.py
+++ RTX_priceChecker.py
@@ -7,24 +7,26 @@ def get_price():
     page = requests.get(
         "https://www.jimms.fi/fi/Product/Show/186256/rog-strix-rtx4080-o16g-gaming/asus-geforce-rtx-4080-rog-strix-oc-edition-naytonohjain-16gb-gddr6x")
     soup = BeautifulSoup(page.content, "html.parser")
-    price = soup.find("span", class_="pricetext").get_text(strip=True)
+    price = soup.find("span", itemprop="price").get_text(strip=True)
     price = price[:-1].replace("\xa0", "").replace(",", ".")
     return float(price)
 
 
 def main():
-    target_price = 1699
+    target_price = 2699
 
     while True:
         price = get_price()
         name = "Asus GeForce RTX 4080 ROG Strix - OC Edition"
-
         if price <= target_price:
             with open("/Users/stremberg_s/Desktop/RTX 4080.txt", "a") as file:
-                file.write("\n" + name + "\t" + str(price))
-            time.sleep(1800)
+                file.write("\n" + name + "\t" + str(price) + " €")
+            time.sleep(3)
+            print("OK")
         else:
-            time.sleep(1800)
+            with open("/Users/stremberg_s/Desktop/RTX 4080.txt", "a") as file:
+                file.write("NOTHING HAPPENING THERE.. :(")
+            time.sleep(3)
 
 
 if __name__ == "__main__":
