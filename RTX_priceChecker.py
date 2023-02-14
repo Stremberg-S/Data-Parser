ec4b44efc8c47d368f0014a2c287f3fa26df9648
Sergei Stremberg <sekispb@hotmail.com>
2023-02-14 18:08:40 +0200

    Bugfix & now main()-method is more friendly with using it with other item

--- RTX_priceChecker.py
+++ RTX_priceChecker.py
@@ -4,35 +4,45 @@ import time
 
 
 def write_to_file(content):
-    with open("/Users/stremberg_s/Desktop/RTX 4080.txt", "a") as file:
-        file.write(content)
+    with open("/Users/stremberg_s/Desktop/RTX_4080.txt", "a") as file:
+        file.write(content + "\n")
 
 
-def get_price():
-    page = requests.get(
-        "https://www.jimms.fi/fi/Product/Show/186256/rog-strix-rtx4080-o16g-gaming/asus-geforce-rtx-4080-rog-strix-oc-edition-naytonohjain-16gb-gddr6x")
+def get_price(url):
+    page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     price = soup.find("span", itemprop="price").get_text(strip=True)
     price = price[:-1].replace("\xa0", "").replace(",", ".")
     return float(price)
 
 
-def main():
-    target_price = 2699
+def get_item(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    item = soup.find("h1", class_="text-normal fs-3 my-0").get_text(strip=True)
+    return item
 
-    while True:
-        price = get_price()
-        name = "Asus GeForce RTX 4080 ROG Strix - OC Edition"
-        if price <= target_price:
-            write_to_file("\n" + name + "\t" + str(price) + " €")
-            time.sleep(3)
-        else:
-            time.sleep(3)
+
+def main(wanted_price, url):
+    price = get_price(url)
+    item = get_item(url)
+    if price < wanted_price:
+        write_to_file(item + "\t" + str(price) + " €")
+        time.sleep(600)
+    else:
+        time.sleep(600)
 
 
 if __name__ == "__main__":
-    try:
-        write_to_file("\n\tSCRIPT STARTED.. :)")
-        main()
-    except:
-        write_to_file("\n\tSCRIPT STOPPED.. :(")
+    write_to_file("\tSCRIPT STARTED.. :)")
+    while True:
+        try:
+            main(1699,
+                 "https://www.jimms.fi/fi/Product/Show/186256/rog-strix-rtx4080-o16g-gaming/asus-geforce-rtx-4080-rog"
+                 "-strix-oc-edition-naytonohjain-16gb-gddr6x")
+            main(1599,
+                 "https://www.jimms.fi/fi/Product/Show/187950/gv-n4080aorus-m-16gd/gigabyte-geforce-rtx-4080-aorus-master"
+                 "-naytonohjain-16gb-gddr6x")
+        except:
+            write_to_file("\tSCRIPT STOPPED.. :(")
+            break
