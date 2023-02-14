8bc4fb7239df6c10d94a7b6809dded25a15ff043
Sergei Stremberg <sekispb@hotmail.com>
2023-02-14 16:48:38 +0200

    new method - write_to_file

--- RTX_priceChecker.py
+++ RTX_priceChecker.py
@@ -3,6 +3,11 @@ from bs4 import BeautifulSoup
 import time
 
 
+def write_to_file(content):
+    with open("/Users/stremberg_s/Desktop/RTX 4080.txt", "a") as file:
+        file.write(content)
+
+
 def get_price():
     page = requests.get(
         "https://www.jimms.fi/fi/Product/Show/186256/rog-strix-rtx4080-o16g-gaming/asus-geforce-rtx-4080-rog-strix-oc-edition-naytonohjain-16gb-gddr6x")
@@ -19,15 +24,15 @@ def main():
         price = get_price()
         name = "Asus GeForce RTX 4080 ROG Strix - OC Edition"
         if price <= target_price:
-            with open("/Users/stremberg_s/Desktop/RTX 4080.txt", "a") as file:
-                file.write("\n" + name + "\t" + str(price) + " €")
+            write_to_file("\n" + name + "\t" + str(price) + " €")
             time.sleep(3)
-            print("OK")
         else:
-            with open("/Users/stremberg_s/Desktop/RTX 4080.txt", "a") as file:
-                file.write("NOTHING HAPPENING THERE.. :(")
             time.sleep(3)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        write_to_file("\n\tSCRIPT STARTED.. :)")
+        main()
+    except:
+        write_to_file("\n\tSCRIPT STOPPED.. :(")
