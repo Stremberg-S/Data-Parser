c11e201212c6289124912c3c194058c5f1b9a8d4
Sergei Stremberg <sekispb@hotmail.com>
2023-03-05 18:45:01 +0200

    added async/await to this script and new discount-method that counts % of reduced price

--- RTX_priceChecker.py
+++ RTX_priceChecker.py
@@ -1,40 +1,52 @@
-import requests
-from bs4 import BeautifulSoup
 from configparser import ParsingError
+from bs4 import BeautifulSoup
+import aiofiles as aiofiles
+import aiohttp
 
 
-def write_to_file(content):
-    with open("/Users/stremberg_s/Desktop/RTX_4080.txt", "a") as file:
-        file.write(content + "\n")
+async def write_to_file(content):
+    async with aiofiles.open("/Users/stremberg_s/Desktop/RTX_4080.txt", "a") as file:
+        await file.write(content + "\n")
 
 
-def get_price(url):
+async def get_price(url):
     try:
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, "html.parser")
-        price = soup.find("span", itemprop="price").get_text(strip=True)
-        price = price[:-1].replace("\xa0", "").replace(",", ".")
-        return float(price)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                content = await response.text()
+                soup = BeautifulSoup(content, "html.parser")
+                price = soup.find("span", itemprop="price").get_text(strip=True)
+                price = price[:-1].replace("\xa0", "").replace(",", ".")
+                return float(price)
     except ParsingError:
-        return write_to_file("\tCan't find the price")
+        return await write_to_file("\tCan't find the price")
 
 
-def get_item(url):
+async def get_item(url):
     try:
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, "html.parser")
-        item = soup.find("h1", class_="text-normal fs-3 my-0").get_text(strip=True)
-        return item
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                content = await response.text()
+                soup = BeautifulSoup(content, "html.parser")
+                item = soup.find("h1", class_="text-normal fs-3 my-0").get_text(strip=True)
+                return item
     except ParsingError:
-        return write_to_file("\tCan't find the item")
+        return await write_to_file("\tCan't find the item")
 
 
-def main(wanted_price, url):
+def discount(old, new):
+    x = (old - new) / old * 100
+    return round(x, 2)
+
+
+async def main(wanted_price, url):
     try:
-        price = get_price(url)
-        item = get_item(url)
-        print(item + "\t" + str(price))
+        price = await get_price(url)
+        item = await get_item(url)
         if price < wanted_price:
-            write_to_file(item + "\t" + str(price) + " €")
+            x = discount(wanted_price, price)
+            await write_to_file(item + "\t" +
+                                str(price) + " €" + "\t-" +
+                                str(x) + "%")
     except ParsingError:
         pass
